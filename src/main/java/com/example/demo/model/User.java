14f25a612dfd15f55e320f7a510cefa9d1c8325f
Vinay288 <vinayhiremath288@gmail.com>
2021-10-31 22:57:25 +0530

    ADDED: user dto with first name and last name

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+}
