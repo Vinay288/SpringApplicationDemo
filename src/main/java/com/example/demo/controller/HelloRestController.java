5e18d70a6e6aa0e96d8e53993fdac9b10b20ddbd
Vinay288 <vinayhiremath288@gmail.com>
2021-10-31 22:23:38 +0530

    ADDED:get rest api call to show hello message

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -2,15 +2,23 @@ package com.example.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
 
-	@RequestMapping(value= {"","/","/home"})
-	public ResponseEntity<String> sayHello(){
-		return new ResponseEntity<String>("Hello world!",HttpStatus.OK);
+	@RequestMapping(value = { "", "/", "/home" })
+	public ResponseEntity<String> sayHello() {
+		return new ResponseEntity<String>("Hello world!", HttpStatus.OK);
 	}
+
 }
